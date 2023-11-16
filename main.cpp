001ae9c8e8cbb42050d2acecc5a597f54c031248
MaristellaRamalho <150091289+MaristellaRamalho@users.noreply.github.com>
2023-11-16 00:10:06 -0300

    Update main.cpp
    Implementando algumas opções do menu

--- main.cpp
+++ main.cpp
@@ -4,31 +4,55 @@
 #include "Musica.h"
 using namespace std;
 
-int main()
-{
-    ListaCadastral Playlit;
-    Cria(Playlit);
-
-    Musica musica1("Gossip", "https://www.youtube.com/watch?v=9IPLXNEzsLc&list=RD9IPLXNEzsLc&start_radio=1");
-    Musica musica2("Style", "https://www.youtube.com/watch?v=-CmadmM5cOk&list=RD9IPLXNEzsLc&index=3&pp=8AUB");
-
-    Inserir(Playlit, musica1);
-    Inserir(Playlit, musica2);
-
-    cout << "Playlist: " << endl;
-    ImprimeLista(Playlit);
-
-    return 0;
+void imprimeMenu() {
+    cout << "-------- MENU -------" << endl;
+    cout << "0 - Sair" << endl;
+    cout << "1 - Adicionar musica" << endl;
+    cout << "2 - Remover musica" << endl;
+    cout << "3 - Tocar primeira musica" << endl;
+    cout << "4 - Ir para proxima musica" << endl;
+    cout << "5 - Voltar para musica anterior" << endl;
+    cout << "6 - Mostrar PlayList" << endl;
 }
 
+int main() {
+    ListaCadastral Playlist;
+    // Criação e inicialização da lista
+
+    imprimeMenu();
+    int opcao;
+
+    do {
+        cout << endl << "opcao: ";
+        cin >> opcao;
+
+        string nomeM, linkM;
+        Musica musica;
 
 
+        switch (opcao) {
+            case 0:
+                cout << "Saindo..." << endl;
+                goto fim_programa;
+            case 1:
+                // Adicionar música
+                cout << "Nome: ";
+                cin >> nomeM;
+                cout << "Link: ";
+                cin >> linkM;
+                musica.setNome(nomeM);
+                musica.setLink(linkM);
+                Inserir(Playlist, musica);
 
+                break;
+            case 6:
+                // Mostrar Playlist
+                ImprimeLista(Playlist);
+                break;
+            // outros cases
+        }
+    } while (opcao != 0);
 
-
-
-
-
-
-
-
+    fim_programa:
+    return 0;
+}
